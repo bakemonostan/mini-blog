47854c00cd13acdfd44ba7c80b59a614392851d8
bakemonostan <ehizomokhomion@gmail.com>
2022-09-30 07:42:31 -0700

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <h1>Welcome Home </h1>
+  <h1>{{ greet }} {{ name }}, </h1>
+  <p>Good to have you here</p>
+  <button @click="handleClick">Le click</button>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  setup(props) {
-    console.log('setup');
-  },
-  created() {
-    console.log('created');
-  },
-  mounted() {
-    console.log('mounted');
+  setup() {
+    let greet = 'Welcome to the home page';
+    let name = 'Ehis';
+
+    const handleClick = () => {
+      console.log('Le Click de la click');
+    };
+
+    return { name, greet, handleClick };
   },
 };
 </script>
