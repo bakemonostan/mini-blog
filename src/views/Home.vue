0df37f7bc244536b49985c5e02fe879aa9d63dc9
bakemonostan <ehizomokhomion@gmail.com>
2022-09-30 07:25:22 -0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Welcome Home </h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  setup(props) {
+    console.log('setup');
+  },
+  created() {
+    console.log('created');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+};
+</script>
+
+<style></style>
